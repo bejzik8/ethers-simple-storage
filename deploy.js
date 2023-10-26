a0da959c83e111cdf0445142c1ed904fe7a8ed1e
Mirko Basic <bejzik8@gmail.com>
2023-10-27 01:41:55 +0200

    Created deploy.js script.

---
+++ deploy.js
@@ -0,0 +1,36 @@
+const ethers = require('ethers')
+const fs = require('fs')
+
+async function main() {
+    // compile contracts in our code
+    // compile contracts separately
+    // http://127.0.0.1:7545
+
+    const provider = new ethers.JsonRpcProvider('http://127.0.0.1:7545')
+
+    const wallet = new ethers.Wallet(
+        '0xb1b6c9a04067280f5db566732cf3bc7418f4eadb58710477d242e03498f608bf',
+        provider
+    )
+
+    const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8')
+    const bynary = fs.readFileSync(
+        './SimpleStorage_sol_SimpleStorage.bin',
+        'utf-8'
+    )
+
+    const contractFactory = new ethers.ContractFactory(abi, bynary, wallet)
+
+    console.log('Deploying contract...')
+
+    const contract = await contractFactory.deploy()
+
+    console.log(contract)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error)
+        process.exit(1)
+    })
